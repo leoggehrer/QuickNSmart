eb9493ccc026bf43850d092851d12cf81a202a3b
Gerhard Gehrer <gerhard.gehrer@indi.systems>
2020-03-08 14:57:27 +0100

    Added AccountController

---
+++ CSharpCodeGenerator.ConApp/Customize/ControllerGenerator.cs
@@ -0,0 +1,23 @@
+﻿//@QnSBaseCode
+//MdStart
+using System;
+using System.Collections.Generic;
+
+namespace CSharpCodeGenerator.ConApp.Generation
+{
+    partial class ControllerGenerator
+    {
+        partial void CreateLogicControllerAttributes(Type type, List<string> codeLines)
+        {
+            if (type.FullName.EndsWith(".Persistence.Account.IIdentity")
+                || type.FullName.EndsWith(".Persistence.Account.IRole")
+                || type.FullName.EndsWith(".Persistence.Account.IIdentityXRole")
+                || type.FullName.EndsWith(".Persistence.Account.ILoginSession")
+                )
+            {
+                codeLines.Add("[Logic.Modules.Security.Authorize(\"SysAdmin\")]");
+            }
+        }
+    }
+}
+//MdEnd
---
+++ CommonBase/Helpers/ExpressionConverter.cs
@@ -0,0 +1,43 @@
+﻿//@QnSBaseCode
+//MdStart
+using System;
+using System.Linq.Expressions;
+
+namespace CommonBase.Helpers
+{
+    public class ExpressionConverter : ExpressionVisitor
+    {
+        private Expression _source;
+        private Expression _dest;
+
+        public ExpressionConverter(Expression source, Expression dest)
+        {
+            _source = source;
+            _dest = dest;
+        }
+
+        public static Expression<Func<TConParam, TConReturn>> ConvertToObject<TParm, TReturn, TConParam, TConReturn>(Expression<Func<TParm, TReturn>> input)
+        {
+            var parm = Expression.Parameter(typeof(TConParam));
+            var castParm = Expression.Convert(parm, typeof(TParm));
+            var body = ReplaceExpression(input.Body, input.Parameters[0], castParm);
+            body = Expression.Convert(body, typeof(TConReturn));
+            return Expression.Lambda<Func<TConParam, TConReturn>>(body, parm);
+        }
+
+        public static Expression ReplaceExpression(Expression body, Expression source, Expression dest)
+        {
+            var replacer = new ExpressionConverter(source, dest);
+            return replacer.Visit(body);
+        }
+
+        public override Expression Visit(Expression node)
+        {
+            if (node == _source)
+                return _dest;
+
+            return base.Visit(node);
+        }
+    }
+}
+//MdEnd
