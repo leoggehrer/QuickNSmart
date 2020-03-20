66626e20f7e6fc041414f5d6d386674cbf77fadf
Gerhard Gehrer <gerhard.gehrer@indi.systems>
2020-03-20 23:17:09 +0100

    Added HtmlExtensions converter from Enum to Html-Select.

---
+++ QuickNSmart.AspMvc/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,43 @@
+﻿//@QnSBaseCode
+//MdStart
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CommonBase.Extensions;
+
+namespace QuickNSmart.AspMvc.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString ToSelect(this IHtmlHelper htmlHelper, string css, string id, string name, IEnumerable<KeyValuePair<Enum, string>> options, Enum selVal)
+        {
+            options.CheckArgument(nameof(options));
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("<select");
+            if (css.HasContent())
+                sb.Append($" class=\"{css}\"");
+
+            if (id.HasContent())
+                sb.Append($" id=\"{id}\"");
+
+            if (name.HasContent())
+                sb.Append($" name=\"{name}\"");
+
+            sb.Append(">");
+            foreach (var item in options)
+            {
+                if (selVal.Equals(item.Key))
+                    sb.AppendLine($"<option value=\"{item.Key}\" selected>{item.Value}</option>");
+                else
+                    sb.AppendLine($"<option value=\"{item.Key}\">{item.Value}</option>");
+            }
+            sb.AppendLine("</select>");
+            return new HtmlString(sb.ToString());
+        }
+    }
+}
+//MdEnd
